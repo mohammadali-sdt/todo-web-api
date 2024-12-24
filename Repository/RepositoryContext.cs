4dc26bf7fdc96258e1cbba57bb9235e3a2fe8dff
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2024-12-24 19:05:49 +0330

    refactor(Repository): integrate RepositoryContext with IdentityDbContext of asp.net core

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,16 +1,20 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repository.Configuration;
 
 namespace Repository
 {
-    public class RepositoryContext : DbContext
+    public class RepositoryContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
 
-        public RepositoryContext(DbContextOptions options): base(options) { }
+        public RepositoryContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new TodoConfiguration());
         }

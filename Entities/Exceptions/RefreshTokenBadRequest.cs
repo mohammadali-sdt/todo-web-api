b905195da80878245c44df63e300f9d3bfd78ce8
MohammadAli Sedaghat <mohammadali.sedaghat.1999@gmail.com>
2024-12-28 21:51:24 +0330

    chore(Exceptions): add RefreshTokenBadRequest

---
+++ Entities/Exceptions/RefreshTokenBadRequest.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Entities.Exceptions;
+
+public class RefreshTokenBadRequest : BadRequestException
+{
+    public RefreshTokenBadRequest() : base("Invalid client request. The tokenDto has some invalid values.") { }
+}
